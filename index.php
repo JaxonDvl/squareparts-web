140f5be829c52234889771a4fef701e8f9cf441e
Alin Tatu <JaxonDvl@users.noreply.github.com>
2016-03-22 13:21:56 +0200

    Added files via upload

---
+++ index.php
@@ -0,0 +1,37 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$database = "squareparts";
+
+// Create connection
+$conn = mysql_connect($servername, $username, $password);
+
+// Check connection
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
+
+$db_selected = mysql_select_db($database, $conn);
+if (!$db_selected) {
+    die ('Can\'t use foo : ' . mysql_error());
+}
+else{
+    echo "Sucessfuly connected to DB";
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Square Parts - WebStore</title>
+</head>
+<body>
+    <p>Hello this is the first page</p>
+<p>later edit</p>
+
+</body>
+</html>
