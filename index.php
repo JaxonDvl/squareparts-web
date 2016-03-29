1892fec7103801a48ab5f8c8309b5804b38c41f6
Alin Tatu <tatualindvl@gmail.com>
2016-03-29 12:19:49 +0300

    added a test link

--- index.php
+++ index.php
@@ -1,26 +1,30 @@
 <?php
     include 'connect.php';
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Square Parts - WebStore</title>
-    
-    <!-- Latest compiled and minified CSS -->
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+    <!DOCTYPE html>
+    <html lang="en">
 
-    <!-- jQuery library -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>Square Parts - WebStore</title>
 
-    <!-- Latest compiled JavaScript -->
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
-</head>
-<body>
-    <div class="container">
-      <h1>Welcome to ITSquare Parts Webshop</h1>
-      <p>This website is build by Alin & Adelin</p> 
-    </div>
-</body>
-</html>
+        <!-- Latest compiled and minified CSS -->
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+
+        <!-- jQuery library -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+
+        <!-- Latest compiled JavaScript -->
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+    </head>
+
+    <body>
+        <a href="#test">this is a test link</a>
+        <div class="container">
+            <h1>Welcome to ITSquare Parts Webshop</h1>
+            <p>This website is build by Alin & Adelin</p>
+        </div>
+    </body>
+
+    </html>
